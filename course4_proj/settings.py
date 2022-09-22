6d17abf58cef1ffd63346d220448bcf7b6d44a21
Vadim Rusu <ruvamd@gmail.com>
2022-09-22 18:31:58 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -52,6 +52,7 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'movies',
         'gh',
+        'django_celery_results'
     ]
 
     MIDDLEWARE = [
@@ -163,3 +164,6 @@ class Dev(Configuration):
     
     # OMDB_KEY = values.SecretValue()
     OMDB_KEY = 'me'
+
+    CELERY_RESULT_BACKEND = "django-db"
+    CELERY_BROKER_URL = "redis://localhost:6379/0"
